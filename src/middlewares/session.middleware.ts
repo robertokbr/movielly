37b6289dd7d5a9247d23a5c0029479767be26b48
robertokbr <robertojuniordev@gmail.com>
2023-03-13 17:04:43 -0300

    feat: initing database

---
+++ src/middlewares/session.middleware.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+export const sessionMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+}
