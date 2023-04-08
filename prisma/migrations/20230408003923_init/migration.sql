54bcb89699ad54bb43c7942ad8fd3c98277982a5
robertokbr <robertojuniordev@gmail.com>
2023-04-08 11:44:07 -0300

    build: clean migrations

--- prisma/migrations/20230408003923_init/migration.sql
+++ prisma/migrations/20230408003923_init/migration.sql
@@ -1,17 +1,20 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "reviews" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "imageUrl" TEXT NOT NULL,
     "rating" INTEGER NOT NULL,
     "comment" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
     "movie" TEXT NOT NULL,
     CONSTRAINT "reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
