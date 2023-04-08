384045075fd02ed6c3b335f0fce3483bda121ae4
robertokbr <robertojuniordev@gmail.com>
2023-04-07 21:12:45 -0300

    refactor: user controllers factories on routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,14 @@
 import { Router } from 'express';
-import { ReviewsController } from '../controllers/reviews.controller';
-import { SessionsController } from '../controllers/sessions.controller';
-import { UsersController } from '../controllers/users.controller';
-import { database } from '../database';
 import { sessionMiddleware } from '../middlewares/session.middleware';
+import { reviewsControllerFactory } from '../controllers/factories/make-reviews.controller.factory';
+import { sessionsControllerFactory } from '../controllers/factories/make-sessions.controller.factory';
+import { usersControllerFactory } from '../controllers/factories/make-users.controller.factory';
 
 export const routes = Router();
-const connection = database.getConnection();
 
-const reviewsController = new ReviewsController(connection);
-const sessionsController = new SessionsController(connection);
-const usersController = new UsersController(connection);
+const reviewsController = reviewsControllerFactory.make();
+const sessionsController = sessionsControllerFactory.make();
+const usersController = usersControllerFactory.make();
 
 routes.post('/reviews', sessionMiddleware, reviewsController.create);
 routes.get('/reviews', reviewsController.list);
